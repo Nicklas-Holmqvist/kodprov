22010636729baa3866f9cee5e50e2d46923b2599
Nicklas-Holmqvist <nicklas_holmqvist@outlook.com>
2022-07-24 10:33:59 +0200

    feat: added got logo

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Image from 'next/image';
 
 import List from './list/List';
 import styles from '../styles/Layout.module.css';
 import Search from './Search';
 import PageSize from './PageSize';
 import Pagination from './pagination/Pagination';
+import gameOfThronesLogo from '../utils/img/png-transparent-a-game-of-thrones-logo-emblem-font-game-of-trones-game-emblem-text.png';
 import { useHousesContext } from '../context/housesContext';
 
 const Layout = () => {
@@ -13,6 +15,9 @@ const Layout = () => {
 
   return (
     <div className={styles.mainSection}>
+      <div className={styles.headerContainer}>
+        <Image src={gameOfThronesLogo} alt="Game of Thrones logo" />
+      </div>
       <div className={styles.listSection}>
         <Search />
         {!loaded && 'Laddar'}
